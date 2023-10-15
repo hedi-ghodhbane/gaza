bdf2aa60e32933d4edc27936528d8416e9ed275e
hedi-ghodhbane <52201138+hedi-ghodhbane@users.noreply.github.com>
2023-10-15 12:31:23 +0100

    remove debounce

--- src/app/hooks/useIsMobile.ts
+++ src/app/hooks/useIsMobile.ts
@@ -1,5 +1,4 @@
 import { useLayoutEffect, useState } from 'react';
-import debounce from 'lodash/debounce';
 
 const useIsMobile = (): boolean => {
     const [isMobile, setIsMobile] = useState(false);
@@ -8,7 +7,7 @@ const useIsMobile = (): boolean => {
         const updateSize = (): void => {
             setIsMobile(window.innerWidth < 768);
         };
-        window.addEventListener('resize', debounce(updateSize, 250));
+        window.addEventListener('resize', updateSize);
         // updateSize();
         return (): void => window.removeEventListener('resize', updateSize);
     }, []);
